77bfa0697a5126db848dc043dba23702fae18a94
Rhys <62643810+Alpacaception@users.noreply.github.com>
2025-09-30 15:28:04 +1000

    Added stock price constructor

--- TradingApp/BackgroundServices/StockPriceService.cs
+++ TradingApp/BackgroundServices/StockPriceService.cs
@@ -1,7 +1,82 @@
-﻿namespace TradingApp.BackgroundServices {
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using TradingApp.Models;
+
+namespace TradingApp.BackgroundServices {
     internal class StockPriceService : BackgroundService {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+        private readonly Stocks _stocks;
+
+        public StockPriceService(ILogger<StockPriceService> logger, IConfiguration configuration, Stocks stocks) {
+            _logger = logger;
+            _configuration = configuration;
+            _stocks = stocks;
+
+            var apiToken = _configuration.GetConnectionString("ApiToken");
+            using HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            // Initalise default stock value
+            foreach (Stock stock in _stocks.StockList.Values) {
+                Uri url = new($"https://finnhub.io/api/v1/quote?symbol={stock.Symbol}&token={apiToken}");
+                HttpResponseMessage? response = httpClient.GetAsync(url).Result;
+                string responseStr = response.Content.ReadAsStringAsync().Result;
+                using JsonDocument doc = JsonDocument.Parse(responseStr);
+                decimal currentPrice = doc.RootElement.GetProperty("c").GetDecimal();
+
+                stock.Price = currentPrice;
+                _logger.LogInformation("{Stock}: ${Price}", stock.Symbol, stock.Price);
+            }
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
-            
+            // Get WSS api key from configuration
+            string? websocketUri = _configuration.GetConnectionString("WebsocketConnection");
+
+            // initialise websocket connection to stock price service
+            using var ws = new ClientWebSocket();
+            Uri uri = new(websocketUri!);
+            await ws.ConnectAsync(uri, CancellationToken.None);
+
+            _logger.LogInformation("Websocket connected");
+
+            // Subscribe to all stocks
+            string msg = "";
+            foreach (string ticker in _stocks.StockList.Keys) {
+                msg = "{\"type\":\"subscribe\",\"symbol\":\"" + ticker + "\"}";
+                _logger.LogInformation("{Msg}", msg);
+                var bytes = Encoding.UTF8.GetBytes(msg);
+                await ws.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+
+
+            var buffer = new byte[1024 * 4];
+            while (!stoppingToken.IsCancellationRequested) {
+                var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Close) {
+                    _logger.LogWarning("Websocket closed");
+                    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+
+                } else if (result.MessageType == WebSocketMessageType.Text) {
+                    // Handle possible fragmentation
+                    var message = new StringBuilder();
+                    message.Append(Encoding.UTF8.GetString(buffer, 0, result.Count));
+
+                    while (!result.EndOfMessage) {
+                        result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                        message.Append(Encoding.UTF8.GetString(buffer, 0, result.Count));
+                    }
+
+                    _logger.LogInformation("Received: {Message}", message);
+                }
+            }
+            _logger.LogError("StockPriceService is stopping");
         }
     }
 }
--- TradingApp/Database setup guide.md
+++ TradingApp/Database setup guide.md
@@ -1,3 +1,12 @@
 ﻿# Setup database connection
 1. Create a file named appsettings.json in the same directory as appsettings.example.json
 2. In appsettings.json change fill in the database information in the connection string
+
+# Setup websocket connection
+1. Create an account on https://finnhub.io/
+2. Get your API key from https://finnhub.io/dashboard
+3. Add it to appsettings.json
+
+# Setup API connection
+1. Follow the websocket connection steps
+2. Put the same API key in appsettings.json under ApiToken
