4a3f098cb8b04fd6f1300e357dfc79b25b1152cc
KiingX14 <kiingx1409@gmail.com>
2025-10-02 02:13:17 +1000

    Update Stock into public class

--- TradingApp/Models/Stock.cs
+++ TradingApp/Models/Stock.cs
@@ -1,14 +1,16 @@
-﻿namespace TradingApp.Models {
-    internal class Stock {
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TradingApp.Models {
+    public class Stock {
         public required string Symbol { get; set; }
         public required string Name { get; set; }
-
         public decimal Price { get; set; }
         public decimal Change { get; set; }
         public decimal ChangePercent { get; set; }
 
         public Stock() { }
 
+        [SetsRequiredMembers]
         public Stock(string symbol, string name, decimal price) {
             Symbol = symbol;
             Name = name;
