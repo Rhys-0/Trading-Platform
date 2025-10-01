da71c0d7cd5bbba5ddebfa933dcac0a29da69a50
KiingX14 <kiingx1409@gmail.com>
2025-10-02 03:34:47 +1000

    Added FinnhubParser Unit Test

---
+++ TradingApp/BackgroundServices/FinnhubParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace TradingApp.BackgroundServices {
+    internal static class FinnhubParser {
+        internal static IEnumerable<(string symbol, decimal price)> ParseTrades(string json) {
+            using var doc = JsonDocument.Parse(json);
+            var root = doc.RootElement;
+
+            if (!root.TryGetProperty("type", out var typeEl) || typeEl.GetString() != "trade")
+                yield break;
+
+            if (!root.TryGetProperty("data", out var dataEl) || dataEl.ValueKind != JsonValueKind.Array)
+                yield break;
+
+            foreach (var t in dataEl.EnumerateArray()) {
+                if (t.TryGetProperty("s", out var sEl) &&
+                    t.TryGetProperty("p", out var pEl) &&
+                    sEl.ValueKind == JsonValueKind.String &&
+                    pEl.ValueKind == JsonValueKind.Number) {
+                    yield return (sEl.GetString()!, pEl.GetDecimal());
+                }
+            }
+        }
+    }
+}
