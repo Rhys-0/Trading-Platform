6279a718ecef5dcb08bb0bdbd440074b0ebacf98
KiingX14 <kiingx1409@gmail.com>
2025-10-22 02:18:26 +1100

    Update Trades so now it is per-user

--- TradingApp/Data/Interfaces/ITradeManager.cs
+++ TradingApp/Data/Interfaces/ITradeManager.cs
@@ -1,11 +1,10 @@
-﻿using TradingApp.Models;
+﻿// Data/Interfaces/ITradeManager.cs
+using TradingApp.Models;
 
 namespace TradingApp.Data.Interfaces {
     public interface ITradeManager {
         Task<bool> AddTradeAsync(Trade trade);
-
-        Task<IEnumerable<Trade>> GetAllTradesAsync();
-
         Task<IEnumerable<Trade>> GetTradesByUserIdAsync(long userId);
+        Task<IEnumerable<Trade>> GetAllTradesAsync();
     }
 }
